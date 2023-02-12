f31a898a5dc9eb93eefdcb1b65b7063f18724a75
Eilon Aspir <eilon.aspir@gmail.com>
2023-02-12 18:18:49 +0200

    deleted old json parser in project.cpp

--- Project.cpp
+++ Project.cpp
@@ -13,16 +13,5 @@ bool Project::runProject(bool vulnArr[]) {
 vulnList Project::readVulns(const std::string& VulnsPath) {
 	vulnList vulns;
 	vulns = convert(VulnsPath);
-	/*std::fstream iFile;
-	iFile.open(VulnsPath, std::ios::in);
-	std::vector<std::string> lines;
-	std::string line;
-	while (std::getline(iFile, line)) {
-		lines.push_back(line);
-	}
-	std::vector<strList> splitted = split(lines);
-	for (int i = 0; i < splitted.size(); i++) {
-		vulns.push_back(convert(parse(splitted[i])));
-	}*/
 	return vulns;
 }
